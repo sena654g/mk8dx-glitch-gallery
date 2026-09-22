f1d0901976e9b1340d03edfde7bd20829cc82555
sena <sena0716j@gmail.com>
2026-09-22 23:40:03 +0900

    r

--- courses-data.js
+++ courses-data.js
@@ -1,156 +1,157 @@
 const courses = {
-    "stuck-waluigi-stadium": {
-        title: "Waluigi Stadium",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "クレーンめがけてキノコを使いジャンプし左側にはじかれます。",
-        description: "Use a Mushroom and jump toward the crane to bounce off to the left side.",
-        video: "vid/stuck/Waluigi-Stadium.mp4"
-    },
 
-    "stuck-athens-dash": {
-        title: "Athens Dash",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "キノコを使いドリフトしながら木にぶつかります。",
-        description: "Drift into the tree while using a Mushroom.",
-        video: "vid/stuck/Athens-Dash.mp4"
-    },
-
-    "stuck-twisted-mansion": {
-        title: "Twisted Mansion",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "200ccで波打つ床の端でキノコを使った後ミニターボを出し中心に向かってジャンプします。左右どちらの道でも可能です。",
-        description: "At 200cc, use a Mushroom near the edge of the wavy floor, then charge Mini-Turbo and jump toward the center. This works on either side of the track.",
-        video: "vid/stuck/Twisted-Mansion.mp4"
-    },
-
-    "stuck-los-angeles-laps": {
-        title: "Los Angeles Laps",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "タイミングよくグライダーに乗り動く物体に当たるとグライダーが閉じます。挟まるとキャラクターが左右に動き続けます。",
-        description: "If you deploy your glider with the right timing and hit a moving object, the glider will close. Once stuck, the character keeps moving left and right.",
-        video: "vid/stuck/Los-Angeles-Laps.mp4"
-    },
-
-    "stuck-merry-mountain": {
-        title: "Merry Mountain",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "コース序盤の橋の上からキノコを使って壁に当たります。",
-        description: "Use a Mushroom on the bridge early in the course and crash into the wall.",
-        video: "vid/stuck/Merry-Mountain.mp4"
-    },
-
-    "stuck-dolphin-shoals": {
-        title: "Dolphin Shoals",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "スティックを下に傾けながら水流に乗り上昇した後クラゲに乗ります。挟まっている間、スティックを傾けるとキャラクターも傾きます。また、キャラクターは上下に揺れています。",
-        description: "Tilt the stick down while riding the water current to rise up, then land on a jellyfish. While stuck, tilting the stick will also tilt the character. Also, the character keeps bobbing up and down.",
-        video: "vid/stuck/Dolphin-Shoals.mp4"
-    },
-
-    "stuck-ribbon-road": {
-        title: "Ribbon Road",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "床の波を利用してSJを使いカートのハンドルにぶつかり下にはじかれます。",
-        description: "Use the floor's wave motion with Kusaan Slide to hit the kart's steering wheel and get bounced downward.",
-        video: "vid/stuck/Ribbon-Road.mp4"
-    },
-
-    "stuck-tick-tock-clock": {
-        title: "Tick-Tock Clock",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "歯車でジャンプアクションした後ゴールに乗ります。",
-        description: "Do a jump action off the gear, then land on the goal.",
-        video: "vid/stuck/Tick-Tock-Clock.mp4"
-    },
-
-    "stuck-sunshine-airport": {
-        title: "Sunshine Airport",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: 'トラックの後ろで<a href="edge-clip.html">Edge Clip</a>を使います。',
-        description: 'Use an <a href="edge-clip.html">Edge Clip</a> behind the truck.',
-        video: "vid/stuck/Sunshine-Airport.mp4"
-    },
-
-    "stuck-coconut-mall": {
-        title: "Coconut Mall",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "岩に向かってキノコを使いジャンプアクションします。",
-        description: "Use a Mushroom and do a jump action toward the rock.",
-        video: "vid/stuck/Coconut-Mall.mp4"
-    },
-
-    "stuck-daisy-cruiser": {
-        title: "Daisy Cruiser",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "デーブルが動いているときに近くで緑甲羅などを使い被弾します。",
-        description: "Get hit by a Green Shell or similar item near the table while it's moving.",
-        video: "vid/stuck/Daisy-Cruiser.mp4"
-    },
-
-    "stuck-grumble-volcano": {
-        title: "Grumble Volcano",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "壁の近くでキノコを使い高くジャンプします。",
-        description: "Use a Mushroom near the wall to jump high.",
-        video: "vid/stuck/Grumble-Volcano.mp4"
-    },
-
-    "stuck-dk-jungle": {
-        title: "DK Jungle",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "キノコを使い崖沿いにある岩に乗ります。",
-        description: "Use a Mushroom and land on the rock along the cliff edge.",
-        video: "vid/stuck/DK-Jungle.mp4"
-    },
-
-    "stuck-dk-summit": {
-        title: "DK Summit",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "ハーフパイプの横へ勢いをつけ乗ります。",
-        description: "Build up speed and get onto the side of the halfpipe.",
-        video: "vid/stuck/DK-Summit.mp4"
-    },
-
-    "stuck-ninja-hideaway": {
-        title: "Ninja Hideaway",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "200㏄で床が上昇する間にキノコを使い奥の看板に乗ります。挟まるとキャラクターが独特な動きをします。",
-        description: "At 200cc, use a Mushroom while the floor is rising to land on the sign in the back. Once stuck, the character makes unusual movements.",
-        video: "vid/stuck/Ninja-Hideaway.mp4"
-    },
-
-    "stuck-piranha-plant-cove": {
-        title: "Piranha Plant Cove",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "水流に乗り上昇しイカリに乗ります。",
-        description: "Ride the water current to rise up, then land on the anchor.",
-        video: "vid/stuck/Piranha-Plant-Cove.mp4"
-    },
-
-      "stuck-dragon-palace": {
-        title: "Dragon Palace",
-        category: "Stuck",
-        categoryLink: "stuck.html",
-        descriptionJa: "小さなキャラとカスタムを選び、境界に向かってジャンプアクションします。",
-        description: "Choose a small character and a custom kart, then perform a jump action toward the boundary.",
-        video: "vid/stuck/Dragon-Palace.mp4"
-    },
+"stuck-waluigi-stadium": { 
+    title: "Waluigi Stadium", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "クレーンめがけてキノコを使いジャンプし左側にはじかれます。", 
+    description: "Use a Mushroom and jump toward the crane to bounce off to the left side.", 
+    video: "vid/stuck/waluigi-stadium.mp4" 
+}, 
+ 
+"stuck-athens-dash": { 
+    title: "Athens Dash", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "キノコを使いドリフトしながら木にぶつかります。", 
+    description: "Drift into the tree while using a Mushroom.", 
+    video: "vid/stuck/athens-dash.mp4" 
+}, 
+ 
+"stuck-twisted-mansion": { 
+    title: "Twisted Mansion", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "200ccで波打つ床の端でキノコを使った後ミニターボを出し中心に向かってジャンプします。左右どちらの道でも可能です。", 
+    description: "At 200cc, use a Mushroom near the edge of the wavy floor, then charge Mini-Turbo and jump toward the center. This works on either side of the track.", 
+    video: "vid/stuck/twisted-mansion.mp4" 
+}, 
+ 
+"stuck-los-angeles-laps": { 
+    title: "Los Angeles Laps", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "タイミングよくグライダーに乗り動く物体に当たるとグライダーが閉じます。挟まるとキャラクターが左右に動き続けます。", 
+    description: "If you deploy your glider with the right timing and hit a moving object, the glider will close. Once stuck, the character keeps moving left and right.", 
+    video: "vid/stuck/los-angeles-laps.mp4" 
+}, 
+ 
+"stuck-merry-mountain": { 
+    title: "Merry Mountain", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "コース序盤の橋の上からキノコを使って壁に当たります。", 
+    description: "Use a Mushroom on the bridge early in the course and crash into the wall.", 
+    video: "vid/stuck/merry-mountain.mp4" 
+}, 
+ 
+"stuck-dolphin-shoals": { 
+    title: "Dolphin Shoals", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "スティックを下に傾けながら水流に乗り上昇した後クラゲに乗ります。挟まっている間、スティックを傾けるとキャラクターも傾きます。また、キャラクターは上下に揺れています。", 
+    description: "Tilt the stick down while riding the water current to rise up, then land on a jellyfish. While stuck, tilting the stick will also tilt the character. Also, the character keeps bobbing up and down.", 
+    video: "vid/stuck/dolphin-shoals.mp4" 
+}, 
+ 
+"stuck-ribbon-road": { 
+    title: "Ribbon Road", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "床の波を利用してSJを使いカートのハンドルにぶつかり下にはじかれます。", 
+    description: "Use the floor's wave motion with Kusaan Slide to hit the kart's steering wheel and get bounced downward.", 
+    video: "vid/stuck/ribbon-road.mp4" 
+}, 
+ 
+"stuck-tick-tock-clock": { 
+    title: "Tick-Tock Clock", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "歯車でジャンプアクションした後ゴールに乗ります。", 
+    description: "Do a jump action off the gear, then land on the goal.", 
+    video: "vid/stuck/tick-tock-clock.mp4" 
+}, 
+ 
+"stuck-sunshine-airport": { 
+    title: "Sunshine Airport", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: 'トラックの後ろで<a href="edge-clip.html">Edge Clip</a>を使います。', 
+    description: 'Use an <a href="edge-clip.html">Edge Clip</a> behind the truck.', 
+    video: "vid/stuck/sunshine-airport.mp4" 
+}, 
+ 
+"stuck-coconut-mall": { 
+    title: "Coconut Mall", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "岩に向かってキノコを使いジャンプアクションします。", 
+    description: "Use a Mushroom and do a jump action toward the rock.", 
+    video: "vid/stuck/coconut-mall.mp4" 
+}, 
+ 
+"stuck-daisy-cruiser": { 
+    title: "Daisy Cruiser", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "デーブルが動いているときに近くで緑甲羅などを使い被弾します。", 
+    description: "Get hit by a Green Shell or similar item near the table while it's moving.", 
+    video: "vid/stuck/daisy-cruiser.mp4" 
+}, 
+ 
+"stuck-grumble-volcano": { 
+    title: "Grumble Volcano", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "壁の近くでキノコを使い高くジャンプします。", 
+    description: "Use a Mushroom near the wall to jump high.", 
+    video: "vid/stuck/grumble-volcano.mp4" 
+}, 
+ 
+"stuck-dk-jungle": { 
+    title: "DK Jungle", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "キノコを使い崖沿いにある岩に乗ります。", 
+    description: "Use a Mushroom and land on the rock along the cliff edge.", 
+    video: "vid/stuck/dk-jungle.mp4" 
+}, 
+ 
+"stuck-dk-summit": { 
+    title: "DK Summit", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "ハーフパイプの横へ勢いをつけ乗ります。", 
+    description: "Build up speed and get onto the side of the halfpipe.", 
+    video: "vid/stuck/dk-summit.mp4" 
+}, 
+ 
+"stuck-ninja-hideaway": { 
+    title: "Ninja Hideaway", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "200㏄で床が上昇する間にキノコを使い奥の看板に乗ります。挟まるとキャラクターが独特な動きをします。", 
+    description: "At 200cc, use a Mushroom while the floor is rising to land on the sign in the back. Once stuck, the character makes unusual movements.", 
+    video: "vid/stuck/ninja-hideaway.mp4" 
+}, 
+ 
+"stuck-piranha-plant-cove": { 
+    title: "Piranha Plant Cove", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "水流に乗り上昇しイカリに乗ります。", 
+    description: "Ride the water current to rise up, then land on the anchor.", 
+    video: "vid/stuck/piranha-plant-cove.mp4" 
+}, 
+ 
+"stuck-dragon-palace": { 
+    title: "Dragon Palace", 
+    category: "Stuck", 
+    categoryLink: "stuck.html", 
+    descriptionJa: "小さなキャラとカスタムを選び、境界に向かってジャンプアクションします。", 
+    description: "Choose a small character and a custom kart, then perform a jump action toward the boundary.", 
+    video: "vid/stuck/dragon-palace.mp4" 
+},
 
     "afterimage-squeaky-clean-sprint": {
         title: "Squeaky Clean Sprint",
@@ -158,7 +159,7 @@ const courses = {
         categoryLink: "afterimage.html",
         descriptionJa: "キノコやターボで加速しながら壁にぶつかり、壁をすり抜けます。",
         description: "Crash into the wall while accelerating with a Mushroom or a turbo boost to clip through it.",
-        video: "vid/afterimage/Squeaky-Clean-Sprint.mp4"
+        video: "vid/afterimage/squeaky-clean-sprint.mp4"
     },
 
     "afterimage-yoshis-island": {
@@ -167,7 +168,7 @@ const courses = {
         categoryLink: "afterimage.html",
         descriptionJa: '<a href="wall-cross.html">Wall Cross</a>を使い裏世界に入った後、残像が発生するエリアを見ることができます。',
         description: 'After entering out of bounds using <a href="wall-cross.html">Wall Cross</a>, you can find an area where the afterimage effect appears.',
-        video: "vid/afterimage/Yoshis-Island.mp4"
+        video: "vid/afterimage/yoshis-island.mp4"
     },
 
     "buried-rainbow-road": {
@@ -176,7 +177,7 @@ const courses = {
         categoryLink: "buried.html",
         descriptionJa: "加速しながら地面に降ります。",
         description: "Descend to the ground while accelerating.",
-        video: "vid/buried/Rainbow-Road.mp4"
+        video: "vid/buried/rainbow-road.mp4"
     },
 
     "buried-sky-high-sundae": {
@@ -185,7 +186,7 @@ const courses = {
         categoryLink: "buried.html",
         descriptionJa: "加速しながら地面に降ります。横を向くことで抜け出せます。",
         description: "Descend to the ground while accelerating. You can escape by turning sideways.",
-        video: "vid/buried/Sky-High-Sundae.mp4"
+        video: "vid/buried/sky-high-sundae.mp4"
     },
 
     "edge-clip-shy-guy-falls": {
@@ -204,5 +205,32 @@ const courses = {
         descriptionJa: "",
         description: "",
         video: "vid/edge-clip/warios-gold-mine.mp4"
+    },
+
+    "edge-clip-kalimari-desert": {
+        title: "Kalimari Desert",
+        category: "Edge Clip",
+        categoryLink: "edge-clip.html",
+        descriptionJa: "",
+        description: "",
+        video: "vid/edge-clip/kalimari-desert.mp4"
+    },
+
+    "edge-clip-dk-jungle": {
+        title: "DK Jungle",
+        category: "Edge Clip",
+        categoryLink: "edge-clip.html",
+        descriptionJa: "",
+        description: "",
+        video: "vid/edge-clip/dk-jungle.mp4"
+    },
+
+    "edge-clip-amsterdam-drift": {
+        title: "Amsterdam DriftS",
+        category: "Edge Clip",
+        categoryLink: "edge-clip.html",
+        descriptionJa: "",
+        description: "",
+        video: "vid/edge-clip/amsterdam-drift.mp4"
     },
 };
